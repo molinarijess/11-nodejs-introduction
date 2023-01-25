606cc830e29ef78e8bf86a86f9c7402fcdebcba5
molinarijess <lacerdajess@gmail.com>
2023-01-25 17:03:58 +0000

    parse incoming data and sending back response lesson

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const fs = require("fs");
+const http = require("http");
 
-fs.readFile("user-data.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log(data.toString());
-  }
+const server = http.createServer((request, response) => {
+  let body = [];
+
+  request.on("data", (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on("end", () => {
+    body = Buffer.concat(body).toString();
+    let userName = "Unknown user";
+    if (body) {
+      userName = body.split("=")[1];
+    }
+    response.setHeader("Content-Type", "text/html");
+    response.write(
+      `<h1>Hi ${userName}</h1>
+      <form method="POST" action="/">
+      <input name="username" type="text">
+      <button type="submit">Send</button></form>`
+    );
+    response.end();
+  });
 });
 
-fs.writeFile("user-data.txt", "username=Max", (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Wrote to file!");
-  }
-});
+server.listen(3000);
