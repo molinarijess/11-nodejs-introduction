29a70cb9dadfc61b316a90758ebc05f1a3a9b211
molinarijess <lacerdajess@gmail.com>
2023-01-26 15:40:15 +0000

    final node lesson

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const locationRoutes = require("./routes/location");
+
 const app = express();
+
+// app.use(bodyParser.json());
+
+//app.use((req, res, next) => {
+// res.setHeader("Access-Control-Allow-Origin", "*");
+// res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+// res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+// next();
+//});
+
+app.use(locationRoutes);
 
 app.set(`view engine`, "ejs");
 app.set("views", "views");
